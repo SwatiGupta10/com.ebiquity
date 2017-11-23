837692c02e11bb54bbbefa6614d70bbfaea52600
Swati Gupta <33030362+SwatiGupta10@users.noreply.github.com>
2017-11-23 15:23:18 +0000

    Update BasePageFieldsGenerator.java

--- src/main/java/com/ebiquity/pages/BasePageFieldsGenerator.java
+++ src/main/java/com/ebiquity/pages/BasePageFieldsGenerator.java
@@ -1,155 +1,59 @@
 /**
- * TestCase.java - A class which contains automated testcase(s) to test
- * multi-user form filling at https://www.ebiquity.com/en/contact-us
+ * BasePageFieldsGenerator.java - A class which contains methods to generate
+ * random text to test the form at https://www.ebiquity.com/en/contact-us
  *
  * @author  Swati Gupta
  * @version 1.0
  */
 
-package test.java.com.ebiquity.testCases;
+package main.java.com.ebiquity.pages;
 
-import main.java.com.ebiquity.pages.BasePageLocators;
-import main.java.com.ebiquity.pages.BasePageFieldsGenerator;
-import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.ITestContext;
-import org.testng.annotations.*;
+import static utils.java.com.ebiquity.RandomUtils.*;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
+public class BasePageFieldsGenerator {
 
-public class TestCase {
-
-    static final String drivers_path =  "resources" + File.separator + "drivers" ;
-    Logger log = Logger.getLogger(TestCase.class);
-
-    WebDriver driver;
-    private String url = "https://www.ebiquity.com/en/contact-us";
-    private BasePageLocators contactUs;
-    private BasePageFieldsGenerator fieldsGen;
-
-    //Instantiate util class objects.
-    @BeforeClass
-    public void setupTest() {
-        fieldsGen = new BasePageFieldsGenerator();
+    public String getHelpText(){
+        return getSentences();
     }
 
-    //Setup Driver
-    @BeforeMethod
-    public void createDriver() {
-        driver = new FirefoxDriver();
-        driver.navigate().to(url);
-        contactUs = new BasePageLocators(driver);
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+    public String getEmailId(){
+        return getEmailAddress();
     }
 
-    //Testcase. Change @invocationCount to run it for multiple Users.
-    @Test(invocationCount = 5, priority=0)
-    public void ebiquityContactUs(ITestContext testContext) {
-        int currentCount = testContext.getAllTestMethods()[0].getCurrentInvocationCount();
-        log.info("Starting Test run number: " + currentCount);
+    public String getFirstName(){
+        return getRandomStringOfLen(10, false);
+    }
 
-        String helpText = fieldsGen.getHelpText();
-        log.info("How can I help you? :" + helpText);
-        contactUs.howCanWeHelp().click();
-        contactUs.howCanWeHelp().sendKeys(helpText);
+    public String getSurname(){
+        return getRandomStringOfLen(12, false);
+    }
 
-        String emailId = fieldsGen.getEmailId();
-        log.info("*Email :" + emailId);
-        contactUs.email().click();
-        contactUs.email().sendKeys(emailId);
+    public String getCompany(){
+        return getRandomStringOfLen(25, false);
+    }
 
-        String firstName = fieldsGen.getFirstName();
-        log.info("*First Name :" + firstName);
-        contactUs.firstName().click();
-        contactUs.firstName().sendKeys(firstName);
+    public String getPosition(){
+        return getRandomStringOfLen(20, false);
+    }
 
-        String surName = fieldsGen.getSurname();
-        log.info("*Surname :" + surName);
-        contactUs.surName().click();
-        contactUs.surName().sendKeys(surName);
+    public int getBusinessAreaIndex(){
+        return getRandomIntBetween(0,11);
+    }
 
-        String company = fieldsGen.getCompany();
-        log.info("*Company :" + company);
-        contactUs.company().click();
-        contactUs.company().sendKeys(company);
+    public String getPhoneNumber(){
+        long phoneNumber = getTenDigitNumber();
+        return Long.toString(phoneNumber);
+    }
 
-        String position = fieldsGen.getPosition();
-        log.info("Position / Job Title :" + position);
-        contactUs.position_JobTitle().click();
-        contactUs.position_JobTitle().sendKeys(position);
+    public int getRegionIndex(){
+        return getRandomIntBetween(1,17);
+    }
 
-        int businessAreaIndex = fieldsGen.getBusinessAreaIndex();
-        log.info("Business Area" + businessAreaIndex);
-        Select business = new Select(contactUs.businessArea());
-        business.selectByIndex(businessAreaIndex);
-
-        String phoneNumber = fieldsGen.getPosition();
-        log.info("Phone Number :" + phoneNumber);
-        contactUs.phoneNumber().click();
-        contactUs.phoneNumber().sendKeys(phoneNumber);
-
-        int regionIndex = fieldsGen.getRegionIndex();
-        log.info("*Your Region :" + regionIndex);
-        Select region = new Select(contactUs.yourRegion());
-        region.selectByIndex(regionIndex);
-
-        boolean isContactByPhone = fieldsGen.isContactByPhone();
-        log.info("Contact me by :" + isContactByPhone);
-        if(isContactByPhone) {
-            contactUs.ContactMeByPhone().click();
+    public boolean isContactByPhone(){
+        float coinTossProbability = getRealBetween0and1();
+        if (coinTossProbability > 0.5){
+            return true;
         }
-        else {
-            contactUs.ContactMeByEmail().click();
-        }
-
-        // Captcha is not handled by Selenium Webdriver.
-
-        log.info("Submit Button");
-        contactUs.contactSubmitButton().click();
-
-        driver.quit();
+        return false;
     }
-
-    //Quit the Driver.
-    @AfterMethod
-    public void quitDriver() {
-        driver.quit();
-    }
-
-    //Release resources util class objects.
-    @AfterClass
-    public void quitSetup() {
-        fieldsGen = null;
-    }
-
-    //Static block to set-up driver according to OS.
-    static {
-         String path = drivers_path;
-         String jvmProcessor = System.getProperty("sun.arch.data.model").trim();
-         String osName = System.getProperty("os.name").toLowerCase();
-         if(osName.indexOf("win") >= 0){
-             if(jvmProcessor.equals("32")){
-                 path = drivers_path + File.separator + "win32";
-             }
-             else{
-                 path = drivers_path + File.separator + "win64";
-             }
-         }
-         else if(osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0 || osName.indexOf("aix") > 0){
-             if(jvmProcessor.equals("32")){
-                 path = drivers_path + File.separator + "linux32";
-             }
-             else{
-                 path = drivers_path + File.separator + "linux64";
-             }
-         }
-         else{
-             path = drivers_path + File.separator + "mac";
-         }
-         System.setProperty("webdriver.gecko.driver", path + File.separator + "geckodriver.exe");
-    }
-
 }
