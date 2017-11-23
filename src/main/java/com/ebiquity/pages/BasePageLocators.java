6b9d0c71a98857ddbe259a9b42f35f9a3336b479
Swati Gupta <33030362+SwatiGupta10@users.noreply.github.com>
2017-11-23 15:23:43 +0000

    Update BasePageLocators.java

--- src/main/java/com/ebiquity/pages/BasePageLocators.java
+++ src/main/java/com/ebiquity/pages/BasePageLocators.java
@@ -1,3 +1,10 @@
+/**
+ * BasePageLocators.java - A class which contains locators for WebElements
+ * of https://www.ebiquity.com/en/contact-us
+ *
+ * @author  Swati Gupta
+ * @version 1.0
+ */
 package main.java.com.ebiquity.pages;
 
 import org.openqa.selenium.By;
@@ -21,13 +28,16 @@ public class BasePageLocators {
     By position_JobTitle = By.id("title");
     //BusinessArea DropDown
     By businessArea = By.id("00Nw0000008iSjL");
+
     By phoneNumber = By.id("phone");
     //yourRegionDropDown
     By yourRegion = By.id("00Nw0000008iSjO");
+
     //Contact me by
     By ContactMeByPhone = By.id("00Nw0000008iSjJ");
     By ContactMeByEmail = By.id("00Nw0000008iSjI");
-    //Submit Button
+
+    By captch = By.xpath(".//*[@id='recaptcha-anchor']/div[5]");
     By contactSubmitButton = By.id("contactSubmit");
 
     public WebElement howCanWeHelp(){ return driver.findElement(howCanWeHelp);}
@@ -36,11 +46,16 @@ public class BasePageLocators {
     public WebElement surName(){ return driver.findElement(surName);}
     public WebElement company(){ return driver.findElement(company);}
     public WebElement position_JobTitle(){ return driver.findElement(position_JobTitle);}
+
     public WebElement businessArea(){ return driver.findElement(businessArea);}
+
     public WebElement phoneNumber(){ return driver.findElement(phoneNumber);}
+
     public WebElement yourRegion(){ return driver.findElement(yourRegion);}
+
     public WebElement ContactMeByPhone(){ return driver.findElement(ContactMeByPhone);}
     public WebElement ContactMeByEmail(){ return driver.findElement(ContactMeByEmail);}
+    public WebElement captch(){ return driver.findElement(captch);}
     public WebElement contactSubmitButton(){ return driver.findElement(contactSubmitButton);}
 
-}
+    }
